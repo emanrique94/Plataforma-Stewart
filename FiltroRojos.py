b40ad35cb8df4b1c015bfc961685ee807a962e8c
Joseph Gutierrez <40409144+JosephKaiden@users.noreply.github.com>
2018-11-06 11:30:42 -0500

    Add files via upload

--- FiltroRojos.py
+++ FiltroRojos.py
@@ -3,23 +3,22 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 while True:
-    _, frame = cap.read()
+    _,frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
 
     #HSV hue set value
-    valorBajoRojo = np.array([150, 150, 150])
+    valorBajoRojo = np.array([125, 125, 125])
     valorAltoRojo = np.array([180, 255, 255])
     mask = cv2.inRange(hsv, valorBajoRojo, valorAltoRojo)
     res = cv2.bitwise_and(frame, frame, mask = mask)
     
-    cv2.imshow("Camara", frame)
-    cv2.imshow("Umbral", mask)
-    cv2.imshow("Resta", res)
+    frame  =  cv2.imshow("Camara", frame)
+    mask = cv2.imshow("mask", mask)
+    res = cv2.imshow("Res", res)
     
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
 cv2.destroyAllWindows()
 cap.release()        
-
