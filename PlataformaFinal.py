7e81ceea3439ce9079f9c3aabca1ee1fcb633888
Joseph Gutierrez <40409144+JosephKaiden@users.noreply.github.com>
2018-11-07 15:11:10 -0500

    Add files via upload

--- PlataformaFinal.py
+++ PlataformaFinal.py
@@ -25,33 +25,33 @@ pwm = Adafruit_PCA9685.PCA9685()
 
 #Pareja 0 y 5
 
-PulsoAlto5 = 1700
+PulsoAlto5 = 1200
 PulsoMedio5 = 1500
-PulsoBajo5 = 1300
+PulsoBajo5 = 1800
 
-PulsoAlto0 = 1700
+PulsoAlto0 = 1800
 PulsoMedio0 = 1500
-PulstoBajo0 = 1300
+PulsoBajo0 = 1200
 
 #Pareja 1 y 2
 
-PulsoAlto1= 1300
+PulsoAlto1= 1200
 PulsoMedio1 = 1500
-PulsoBajo1 = 1700
+PulsoBajo1 = 1800
 
-PulsoAlto2 = 1700
+PulsoAlto2 = 1800
 PulsoMedio2 = 1500
-PulsoBajo2 = 1300
+PulsoBajo2 = 1200
 
 #Pareja 3 y 4 
 
-PulsoAlto4 = 1700
+PulsoAlto4 = 1800
 PulsoMedio4 = 1500
-PulsoBajo4 = 1300
+PulsoBajo4 = 1200
 
-PulsoAlto3 = 1700
+PulsoAlto3 = 1200
 PulsoMedio3 = 1500
-PulsoBajo3 = 1300
+PulsoBajo3 = 1800
 
 # Helper function to make setting a servo pulse width simpler.
 def set_servo_pulse(channel, pulse):
@@ -63,45 +63,96 @@ def set_servo_pulse(channel, pulse):
     pulse *= 1000
     pulse //= pulse_length
     pwm.set_pwm(channel, 0, pulse)
+    
+def posicionInicial():
+    #parejas 0 y 5
+    pulso0normalchanel0 = int(PulsoMedio0 / (1000000/60) * 4095)
+    pulso0normalchanel5 = int(PulsoMedio5 / (1000000/60) * 4095)
+    
+    # parejas  1 y 2
+    pulso0normalchanel1 = int(PulsoMedio1 / (1000000/60) * 4095)
+    pulso0normalchanel2 = int(PulsoMedio2 / (1000000/60) * 4095)
+    
+    # parejas 3 y 4
+    pulso0normalchanel3 = int(PulsoMedio3 / (1000000/60) * 4095)
+    pulso0normalchanel4 = int(PulsoMedio4 / (1000000/60) * 4095)
+    #motion
+    
+    pwm.set_pwm(1, 0, pulso0normalchanel1)
+    time.sleep(1)
+    pwm.set_pwm(2, 0, pulso0normalchanel2)
+    time.sleep(1)
+    pwm.set_pwm(4, 0, pulso0normalchanel4)
+    time.sleep(1)
+    pwm.set_pwm(3, 0, pulso0normalchanel3)
+    time.sleep(1)
+    pwm.set_pwm(5, 0, pulso0normalchanel5)
+    time.sleep(1)
+    pwm.set_pwm(0, 0, pulso0normalchanel0)
+    time.sleep(1)
+    
+def posicionAlta12():
+       #parejas 0 y 5
+    pulso0normalchanel0 = int(PulsoBajo0 / (1000000/60) * 4095)
+    pulso0normalchanel5 = int(PulsoBajo5 / (1000000/60) * 4095)
+    
+    # parejas  1 y 2
+    pulso0normalchanel1 = int(PulsoAlto1 / (1000000/60) * 4095)
+    pulso0normalchanel2 = int(PulsoAlto2 / (1000000/60) * 4095)
+    
+    # parejas 3 y 4
+    pulso0normalchanel3 = int(PulsoBajo3 / (1000000/60) * 4095)
+    pulso0normalchanel4 = int(PulsoBajo4 / (1000000/60) * 4095)
+    #motion
 
+    pwm.set_pwm(1, 0, pulso0normalchanel1)
+    time.sleep(2)
+    pwm.set_pwm(2, 0, pulso0normalchanel2)
+    time.sleep(2)
+    pwm.set_pwm(3, 0, pulso0normalchanel3)
+    time.sleep(2)
+    pwm.set_pwm(4, 0, pulso0normalchanel4)
+    time.sleep(2)
+    pwm.set_pwm(0, 0, pulso0normalchanel0)
+    time.sleep(2)
+    pwm.set_pwm(5, 0, pulso0normalchanel5)
+    time.sleep(2)
+    
+def posicionAlta05():
+    #parejas 0 y 5
+    pulso0normalchanel0 = int(PulsoAlto0 / (1000000/60) * 4095)
+    pulso0normalchanel5 = int(PulsoAlto5 / (1000000/60) * 4095)
+    
+    # parejas  1 y 2
+    pulso0normalchanel1 = int(PulsoBajo1 / (1000000/60) * 4095)
+    pulso0normalchanel2 = int(PulsoBajo2 / (1000000/60) * 4095)
+    
+    # parejas 3 y 4
+    pulso0normalchanel3 = int(PulsoBajo3 / (1000000/60) * 4095)
+    pulso0normalchanel4 = int(PulsoBajo4 / (1000000/60) * 4095)
+    #motion
+    pwm.set_pwm(5, 0, pulso0normalchanel5)
+    time.sleep(2)
+    pwm.set_pwm(0, 0, pulso0normalchanel0)
+    time.sleep(2)
+    pwm.set_pwm(2, 0, pulso0normalchanel2)
+    time.sleep(2)
+    pwm.set_pwm(1, 0, pulso0normalchanel1)
+    time.sleep(2)
+    pwm.set_pwm(4, 0, pulso0normalchanel4)
+    time.sleep(2)
+    pwm.set_pwm(3, 0, pulso0normalchanel3)
+    time.sleep(2)
+    
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
 
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 while True:
-    # parejas 0 y 5
-    pulso0 = 1400 # para valores menores a 1500 baja 
-    pulso0normal = int(pulso0 / (1000000/60) * 4095)
-    
-    pulso0chanel5 = 1600 # para valores menores a 1500 baja
-    pulso0normalchanel5 = int(pulso0chanel5 / (1000000/60) * 4095)
+    #move servos
+    posicionInicial()
+    time.sleep(5)
+    posicionAlta12()
 
-    
-    # parejas 1 y 2
-    pulso0chanel1 = 1700 #servo 1 para valores mayores a 1500 baja
-    pulso0normalchanel1 = int(pulso0chanel1 / (1000000/60) * 4095)
-    
-    pulso0chanel2 = 1300 # para valores menores a 1500 baja
-    pulso0normalchanel2 = int(pulso0chanel2 / (1000000/60) * 4095)
-    
-    # parejas 3 y 4
-    pulso0chanel3 = 1700 # para valores mayores a 1500 baja
-    pulso0normalchanel3 = int(pulso0chanel3 / (1000000/60) * 4095)
-    
-    pulso0chanel4 =  1300 # para valores menores a 1500 baja
-    pulso0normalchanel4 = int(pulso0chanel4 / (1000000/60) * 4095)
-    
-        
-    
-    
-# Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, pulso0normal)
-    time.sleep(1)
-    pwm.set_pwm(1, 0, pulso0normalchanel1)
-    pwm.set_pwm(2, 0, pulso0normalchanel2)
-    pwm.set_pwm(3, 0, pulso0normalchanel3)
-    pwm.set_pwm(4, 0, pulso0normalchanel4)
-    pwm.set_pwm(5, 0, pulso0normalchanel5)
-    time.sleep(1)
-    #pwm.set_pwm(0, 0, 2000)
-    #time.sleep(1)
+
+   
